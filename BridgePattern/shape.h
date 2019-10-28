606ba0c531a45140544feb2c572cb2f0563827d8
lingaojun <44394422+lingaojun@users.noreply.github.com>
2019-10-28 20:28:24 +0800

    Update shape.h

--- BridgePattern/shape.h
+++ BridgePattern/shape.h
@@ -1,6 +1,15 @@
-#ifndef __SHAPE_H#define __SHAPE_H
-#include "drawAPI.h"
-class Shape {public:    Shape(DrawAPI *drawAPI);    virtual ~Shape();    virtual void draw() = 0;   
-DrawAPI *m_drawAPI;};
+#ifndef __SHAPE_H
+#define __SHAPE_H 
+
+#include "drawAPI.h"
+
+class Shape {
+public:
+    Shape(DrawAPI *drawAPI);
+    virtual ~Shape();
+    virtual void draw() = 0;
+   
+DrawAPI *m_drawAPI;
+};
 
 #endif
